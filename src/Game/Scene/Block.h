87e213cae135eac753198a2969c9e69f488681f5
504-15\cre <a>
2025-06-21 17:28:48 +0900

    ブロックのクラスとコリジョン設定。円の描画。

---
+++ src/Game/Scene/Block.h
@@ -0,0 +1,27 @@
+﻿//---------------------------------------------------------------------------
+//!	@file	Block.h
+//! @brief	ゲームメイン
+//---------------------------------------------------------------------------
+#pragma once
+//===========================================================================
+//! アニメーションサンプルシーン
+//===========================================================================
+#include <System/Scene.h>
+
+USING_PTR(Block);
+
+class Block : public Object
+{
+public:
+    BP_OBJECT_DECL(Block, u8"プレイシーンのカメラ")
+
+    //@
+    bool Init() override;      //!< 初期化
+    void Update() override;    //!< 更新
+    void Draw() override;      //!< 描画
+    void Exit() override;      //!< 終了
+    void GUI() override;       //!< GUI表示
+
+private:
+    const float RADUIS_ = 2.0f;
+};
