86ad1f30e2e79e20eb1aca4f9a4c4cf27b5eb631
502-31\cre <oca2402sg0009@edu.oca.ac.jp>
2025-06-25 09:13:45 +0900

    エネミーのうつ伏せの実装、しゃがみの高さの調整

--- src/Game/Scene/Enemy.h
+++ src/Game/Scene/Enemy.h
@@ -23,16 +23,20 @@ public:
     void GUI() override;       //!< GUI表示
 
 private:
-    const float RADIUS_         = 2.0f;           //半径
-    const float TOP_POINT_      = RADIUS_ * 3;    //しゃがんでいない頭の位置
-    const int   PUT_RADIUS_MAX_ = 50;             //出現する範囲の最大
+    const float RADIUS_              = 2.0f;           //半径
+    const float TOP_POINT_           = RADIUS_ * 3;    //しゃがんでいない頭の位置
+    const float SQUAT_TOP_POINT_     = RADIUS_ * 2;    //しゃがんでいるときの頭の位置
+    const float FACE_DOWN_TOP_POINT_ = RADIUS_;        //うつ伏せの時の頭の位置
+    const int   PUT_RADIUS_MAX_      = 50;             //出現する範囲の最大
 
-    float neutral_pos_ = TOP_POINT_;    //円の位置の高さ
+    float neutral_pos_  = TOP_POINT_;    //円の位置の高さ
+    bool  is_face_down_ = false;         //うつ伏せの状態かを確認する用の変数
 
     //以下のものはAIができたら消してください
-    const int SQUAT_TIME_MIN_ = 60;                      //しゃがむ時間の最小時間
-    const int SQUAT_TIME_MAX_ = SQUAT_TIME_MIN_ * 10;    //しゃがむ時間の最大時間
+    const int RANDOM_TIME_MIN_ = 60;                       //しゃがむ時間の最小時間
+    const int RANDOM_TIME_MAX_ = RANDOM_TIME_MIN_ * 10;    //しゃがむ時間の最大時間
 
-    int squat_timer_;    //しゃがむ時間
-    int jump_timer_;     //ジャンプする時間
+    int squat_timer_;        //しゃがむ時間
+    int jump_timer_;         //ジャンプする時間
+    int face_down_timer_;    //ジャンプする時間
 };
