d283071e3175fce9e5ce954c752845658da4f2e2
504-12\cre <a>
2025-06-28 19:21:23 +0900

    [Add]ComponentLiftable

--- src/Game/Scene/Player.cpp
+++ src/Game/Scene/Player.cpp
@@ -8,6 +8,7 @@
 #include <System/Component/ComponentJump.h>
 #include <System/Component/ComponentLift.h>
 #include <System/Component/ComponentRigidbody.h>
+#include <System/Component/ComponentLiftable.h>
 
 //---------------------------------------------------------------------------------
 //!	初期化
@@ -15,7 +16,8 @@
 bool Player::Init()
 {
     __super::Init();
-    AddComponent<ComponentRigidbody>();
+    AddComponent<ComponentRigidbody>();    //剛体コンポーネントを追加
+    AddComponent<ComponentLiftable>();     //持ち上げられ機能コンポーネント
     auto object_controller_comp = AddComponent<ComponentObjectController>();
     object_controller_comp->SetMoveSpeed(0.2f);
     object_controller_comp->SetRotateSpeed(20.0f);
---
+++ src/System/Component/ComponentLiftable.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include <System/Scene.h>
+#include <System/Component/Component.h>
+
+USING_PTR(ComponentLiftable);
+
+//! @持ち上げ機能コンポーネント
+class ComponentLiftable : public Component
+{
+public:
+    BP_COMPONENT_DECL(ComponentLiftable, u8"持ち上げコンポーネント");
+    //! @{
+    // 初期化処理
+    void Init() override;
+
+    // 更新処理
+    void Update() override;
+
+    //ImGui
+    void GUI() override;
+
+    //持ち上げられているかの状態を変更
+    void SetLiftedFlag(bool lifted_flag);
+
+    //持ち上げられているか否かを返す関数
+    bool IsLifted();
+    //! @}
+
+private:
+    bool is_lifted_;    //持ち上げられているか否かを保持する変数
+
+    //--------------------------------------------------------------------
+    //! @name Cereal処理
+    //--------------------------------------------------------------------
+    //@{
+
+    //! @brief セーブ
+    // @param arc アーカイバ
+    // @param ver バージョン
+    CEREAL_SAVELOAD(arc, ver) { arc(cereal::make_nvp("Component", cereal::base_class<Component>(this))); }
+};
+
+CEREAL_CLASS_VERSION(ComponentLiftable, 3);
