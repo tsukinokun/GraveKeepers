c7465ec2bfa974581ecf4e22136c3fbbf416180c
502-29\cre <maihei541@gmail.com>
2025-07-02 14:07:21 +0900

    [Fix]If player is Lifted,player can't jump.

--- src/System/Component/ComponentJump.cpp
+++ src/System/Component/ComponentJump.cpp
@@ -6,6 +6,7 @@
 #pragma once
 #include <System/Component/ComponentJump.h>
 #include <System/Component/ComponentRigidbody.h>
+#include <System/Component/ComponentLiftable.h>
 
 //---------------------------------------------------------------------------
 //! @brief	初期化処理
@@ -23,7 +24,9 @@ void ComponentJump::Update()
     __super::Update();
     jump_frame_count_--;
     auto owner = GetOwner();
-
+    //オーナーが持ち上げられ状態なら、後の処理は行わない。
+    if(owner->GetComponent<ComponentLiftable>()->IsLifted())
+        return;
     //スペースキー押下でジャンプ
     if(conditions_jump_() && (jump_frame_count_ < 0) && set_enable_ == false) {
         jump_frame_count_ = jump_frame_max_;
