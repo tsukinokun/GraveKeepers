c93d0d618ddc4518b0319be3b05ba70175874fdf
502-29\cre <maihei541@gmail.com>
2025-07-23 16:35:07 +0900

    [Add]TextClass

--- src/Game/Scene/UIObject/UIText.h
+++ src/Game/Scene/UIObject/UIText.h
@@ -1,24 +1,22 @@
 ﻿//---------------------------------------------------------------------------
-//!	@file	Wall.h
-//! @brief	壁
+//!	@file	UIText.h
+//! @brief	UIの文字オブジェクト
+//! @author
 //---------------------------------------------------------------------------
 #pragma once
 #include <System/Scene.h>
 
-USING_PTR(Wall);
+USING_PTR(UIText);
 
-class Wall : public Object
+class UIText : public Object
 {
 public:
-    BP_OBJECT_DECL(Wall, u8"プレイシーンの壁")
+    BP_OBJECT_DECL(UIText, u8"UIの文字オブジェクト")
 
-    //@
+    //@}
     bool Init() override;      //!< 初期化
     void Update() override;    //!< 更新
     void Draw() override;      //!< 描画
     void Exit() override;      //!< 終了
     void GUI() override;       //!< GUI表示
-
-private:
-    const float RADUIS_ = 2.0f;
 };
