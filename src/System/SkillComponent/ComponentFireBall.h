42e2e3e6fb50817f748416f7d1e30257a740b9c5
tsukinokun <maihei541@gmail.com>
2025-07-31 23:07:08 +0900

    [Fix]WallModel

---
+++ src/System/SkillComponent/ComponentFireBall.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <System/Scene.h>
+#include <System/Component/Component.h>
+
+USING_PTR(ComponentFireBall);
+
+class ComponentFireBall : public Component
+{
+public:
+    BP_COMPONENT_DECL(ComponentFireBall, u8"ファイアボールのコンポーネント");
+    //! @{
+    //	初期化処理
+    void Init() override;
+
+    //	物理演算
+    void LateDraw() override;
+
+    //ImGui
+    void GUI() override;
+    //! @}
+
+private:
+    //--------------------------------------------------------------------
+    //! @name Cereal処理
+    //--------------------------------------------------------------------
+    //@{
+
+    //! @brief セーブ
+    // @param arc アーカイバ
+    // @param ver バージョン
+    CEREAL_SAVELOAD(arc, ver) { arc(cereal::make_nvp("Component", cereal::base_class<Component>(this))); }
+};
+
+CEREAL_CLASS_VERSION(ComponentFireBall, 3);
