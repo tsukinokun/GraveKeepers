93c4ce4b26fbdcc5f9ed7b3f26f35caf1b61e2d0
tsukinokun <maihei541@gmail.com>
2025-08-10 00:50:46 +0900

    [Add]UIImageObject

---
+++ src/System/UIComponent/ComponentImage.cpp
@@ -0,0 +1,96 @@
+﻿//---------------------------------------------------------------------------
+//!	@file	ComponentImage.cpp
+//! @brief	画像コンポーネント
+//! @auther 山﨑愛
+//---------------------------------------------------------------------------
+#include "ComponentImage.h"
+#include "ComponentTransformUI.h"
+
+//---------------------------------------------------------------------------
+//! @brief	初期化関数
+//---------------------------------------------------------------------------
+void ComponentImage::Init()
+{    // 初期化処理
+    __super::Init();
+}
+
+//---------------------------------------------------------------------------
+//! @brief	UI描画
+//---------------------------------------------------------------------------
+void ComponentImage::LateDraw()
+{
+    __super::LateDraw();
+    auto   owner      = GetOwner();                  //オーナーを取得
+    float3 adjustment = float3(0.0f, 0.0f, 0.0f);    // 調整値(Alignmentに合わせて)
+    if(auto comp_transform = owner->GetComponent<ComponentTransformUI>()) {
+        ComponentTransformUI::Alignment alignment = comp_transform->GetAlignment();
+        float                           width     = 0;    // 幅
+        float                           hight     = 0;    //高さ
+        GetGraphSizeF(image_, &width, &hight);
+        //配置位置(縦)
+        switch(static_cast<int>(alignment) / 3) {
+        case 0:
+            adjustment.y = (-hight * 0.5f);
+            break;    // 上寄せ
+        case 1:
+            adjustment.y = 0.0f;
+            break;    // 中央寄せ
+        case 2:
+            adjustment.y = (hight * 0.5f);
+            break;    // 下寄せ
+        }
+        //配置位置(横)
+        switch(static_cast<int>(alignment) % 3) {
+        case 0:
+            adjustment.x = (-width * 0.5f);
+            break;    // 左寄せ
+        case 1:
+            adjustment.x = 0.0f;
+            break;    // 中央寄せ
+        case 2:
+            adjustment.x = (width * 0.5f);
+            break;    // 右寄せ
+        }
+    }
+    float3 pos   = float3(0.0f, 0.0f, 0.0f);
+    pos          = owner->GetTranslate() + adjustment;
+    float3 scale = owner->GetScaleAxisXYZ();
+    //サイズは、Transformの平均
+    float size = (scale.x + scale.y + scale.z) / 3.0f;    // 平均値をとる
+    //角度はx軸の角度から取る
+    float angle = owner->GetRotationAxisXYZ().x;    // X軸の角度を取得
+    DrawRotaGraphF(pos.x, pos.y, static_cast<double>(size), static_cast<double>(angle), image_, TRUE);
+}
+
+//---------------------------------------------------------------------------
+//! @brief	ImGui
+//---------------------------------------------------------------------------
+void ComponentImage::GUI()
+{
+    __super::GUI();
+
+    // GUI内に出現させる
+    ImGui::Begin(GetOwner()->GetName().data());
+    {
+        ImGui::Separator();
+        if(ImGui::TreeNode(u8"文字列コンポーネント")) {
+            if(ImGui::Button(u8"削除"))
+                GetOwner()->RemoveComponent(shared_from_this());
+
+            ImGui::TreePop();
+        }
+    }
+    ImGui::End();
+}
+
+//---------------------------------------------------------------------------
+//! @brief	文字列の設定
+//---------------------------------------------------------------------------
+std::shared_ptr<ComponentImage> ComponentImage::SetImage(int image)
+{
+    image_ = image;    // 画像ハンドルを設定
+    return dynamic_pointer_cast<ComponentImage>(shared_from_this());
+}
+
+CEREAL_REGISTER_TYPE(ComponentImage)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(Component, ComponentImage)
---
+++ src/System/UIComponent/ComponentImage.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include <System/Scene.h>
+#include <System/Component/Component.h>
+
+USING_PTR(ComponentImage);
+
+class ComponentImage : public Component
+{
+public:
+    BP_COMPONENT_DECL(ComponentImage, u8"画像コンポーネント");
+    //! @{
+    //	初期化処理
+    void Init() override;
+
+    //描画処理
+    void LateDraw() override;
+
+    //ImGui
+    void GUI() override;
+
+    //画像の設定
+    //! @param image [in] 描画する画像ハンドル
+    //! @retval 自身のポインタ
+    std::shared_ptr<ComponentImage> SetImage(int image);
+    //! @}
+
+private:
+    int image_ = -1;    //!< 描画する画像ハンドル
+
+    //--------------------------------------------------------------------
+    //! @name Cereal処理
+    //--------------------------------------------------------------------
+    //@{
+
+    //! @brief セーブ
+    // @param arc アーカイバ
+    // @param ver バージョン
+    CEREAL_SAVELOAD(arc, ver) { arc(cereal::make_nvp("Component", cereal::base_class<Component>(this))); }
+};
+
+CEREAL_CLASS_VERSION(ComponentImage, 3);
