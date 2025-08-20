7c0013722bfb6585374cf4dd3db92f9528fa5b3a
玉葱\tiger <oca2402sg0009@edu.oca.ac.jp>
2025-08-21 01:56:04 +0900

    狼男のクラスの作成

---
+++ src/Game/Scene/Character/Werewolf/Werewolf.cpp
@@ -0,0 +1,61 @@
+﻿//---------------------------------------------------------------------------
+//!	@file	Werewolf.cpp
+//! @brief	インゲームシーンのゾンビ
+//! @author 山﨑愛
+//---------------------------------------------------------------------------
+#include "Werewolf.h"
+#include <System/Component/ComponentObjectController.h>
+#include <System/Component/ComponentCollisionCapsule.h>
+#include <System/Component/ComponentModel.h>
+#include <System/Component/ComponentJump.h>
+#include <System/Component/ComponentLift.h>
+#include <System/Component/ComponentRigidbody.h>
+#include <System/Component/ComponentLiftable.h>
+#include <System/Component/ComponentStatus.h>
+
+//---------------------------------------------------------------------------------
+//!	初期化
+//---------------------------------------------------------------------------------
+bool Werewolf::Init()
+{
+    __super::Init();
+    if(auto model_comp = model_component_.lock()) {
+        model_comp->Load("data/PoyPoy/Model/Character/Werewolf/Werewolf.mv1");
+    }
+    if(auto status_comp = status_component_.lock()) {
+        status_comp->SetHitPoints(HP_MAX_);
+        status_comp->SetSpeed(MOVE_SPEED_);
+    }
+    SetName(u8"狼男");
+    return true;
+}
+
+//---------------------------------------------------------------------------------
+//!	更新
+//---------------------------------------------------------------------------------
+void Werewolf::Update()
+{
+    __super::Update();
+}
+
+//---------------------------------------------------------------------------------
+//!	描画
+//---------------------------------------------------------------------------------
+void Werewolf::Draw()
+{
+    __super::Draw();
+}
+
+//---------------------------------------------------------------------------------
+//!	終了
+//---------------------------------------------------------------------------------
+void Werewolf::Exit()
+{
+    __super::Exit();
+}
+
+//!GUI表示
+void Werewolf::GUI()
+{
+    __super::GUI();
+}
---
+++ src/Game/Scene/Character/Werewolf/Werewolf.h
@@ -0,0 +1,26 @@
+﻿//---------------------------------------------------------------------------
+//!	@file	Werewolf.h
+//! @brief	インゲームシーンの狼男
+//! @author
+//---------------------------------------------------------------------------
+#pragma once
+#include <System/Scene.h>
+#include <Game/Scene/Character/Base/Character.h>
+USING_PTR(Werewolf);
+
+class Werewolf : public Character
+{
+public:
+    BP_OBJECT_DECL(Werewolf, u8"インゲームの狼男")
+
+    //@
+    bool Init() override;      //!< 初期化
+    void Update() override;    //!< 更新
+    void Draw() override;      //!< 描画
+    void Exit() override;      //!< 終了
+    void GUI() override;       //!< GUI表示
+
+private:
+    const int   HP_MAX_     = 200;     //HPの最大値
+    const float MOVE_SPEED_ = 0.2f;    //移動スピード
+};
