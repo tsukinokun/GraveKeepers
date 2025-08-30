8ce9b321dc6adfc6eb840f22f64b09d9c603ee73
504-14\cre <a>
2025-08-30 17:30:43 +0900

    [Fix]投げた時にコリジョンに引っかかるバグを修正

--- src/System/Component/ComponentLift.cpp
+++ src/System/Component/ComponentLift.cpp
@@ -36,16 +36,17 @@ void ComponentLift::Update()
         lift_obj->GetComponent<ComponentLiftable>()->SetLiftedFlag(true);
         auto   owner_col  = owner->GetComponent<ComponentCollisionCapsule>();    //オーナーのコリジョンを取得
         float3 end        = owner->GetTranslate();                               //高さ
-        end.y            += (owner_col->GetHeight() + 2.0f);                     //終点座標は頭なので、高さの半分を足す。
+        end.y            += (owner_col->GetHeight() + 4.0f);                     //終点座標は頭なので、高さの半分を足す。
         //持ち上げ対象を持ち上げる
         lift_obj->SetTranslate(end);
 
         //投げる
         if(conditions_for_throw_()) {
             if(auto lift_obj = lift_object_.lock()) {
-                auto   lift_rb        = lift_obj->GetComponent<ComponentRigidbody>();
-                float3 throw_impulse_ = float3(0.0f, throw_virtical_power_, 0.0f);
-                float3 owner_rot      = owner->GetRotationAxisXYZ();    //オーナーの向きを取得
+                auto   lift_rb         = lift_obj->GetComponent<ComponentRigidbody>();
+                float3 throw_impulse_  = float3(0.0f, throw_virtical_power_, 0.0f);
+                float3 owner_rot       = owner->GetRotationAxisXYZ();    //オーナーの向きを取得
+                owner_rot.y           += 180.0f;                         //座標系の関係でyを180度回転する、オブジェクトの背中が正面
                 //オーナーのy軸回転から、throw_impulse_のxとzを設定
                 throw_impulse_.x = -throw_horizontal_power_ * sinf(D2R(owner_rot.y));
                 throw_impulse_.z = -throw_horizontal_power_ * cosf(D2R(owner_rot.y));
