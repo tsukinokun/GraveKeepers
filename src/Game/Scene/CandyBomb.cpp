9696f1e4f9fdd77c0c09b53dbd5dd36633549827
504-20\cre <a>
2025-09-06 19:35:24 +0900

    キャンディ爆弾見た目の実装、テクスチャはがれている

---
+++ src/Game/Scene/CandyBomb.cpp
@@ -0,0 +1,69 @@
+﻿//---------------------------------------------------------------------------
+//!	@file	Block.cpp
+//! @brief	ゲームシーンのブロックオブジェクト
+//---------------------------------------------------------------------------
+#include "CandyBomb.h"
+#include <System/RandomRange/RandomRange.h>
+#include <System/Component/ComponentModel.h>
+#include <System/Component/ComponentCollisionSphere.h>
+#include <System/Component/ComponentRigidbody.h>
+#include <System/Component/ComponentLiftable.h>
+
+//---------------------------------------------------------------------------------
+//!	初期化
+//---------------------------------------------------------------------------------
+bool CandyBomb::Init()
+{
+    __super::Init();
+
+    SetName(u8"キャンディー爆弾");
+
+    //球のコリジョンをアタッチ（モデルの形通りにするとすり抜けたため形通りにはしていません）
+    auto candy_col = AddComponent<ComponentCollisionSphere>();
+    //block_col->UseGravity();
+    // コリジョンの半径を設定
+    candy_col->SetRadius(RADUIS_);
+
+    AddComponent<ComponentModel>("data/PoyPoy/Model/Object/CandyBomb/CandyBomb.mv1");
+
+    //モデルコンポーネントを取得
+    auto obj = GetComponent<ComponentModel>();
+    obj->SetScaleAxisXYZ(SCALE_);    //サイズの設定
+    //位置の設定（-DISTANCE_RANGE_からDISTANCE_RANGE_の間に設置）
+    SetTranslate(float3(GetRandomRangeF(-DISTANCE_RANGE_, DISTANCE_RANGE_), 0.0f, GetRandomRangeF(-DISTANCE_RANGE_, DISTANCE_RANGE_)));
+
+    auto rb = AddComponent<ComponentRigidbody>();
+    AddComponent<ComponentLiftable>();    //持ち上げられ機能コンポーネント
+
+    return true;
+}
+
+//---------------------------------------------------------------------------------
+//!	更新
+//---------------------------------------------------------------------------------
+void CandyBomb::Update()
+{
+    __super::Update();
+}
+
+//---------------------------------------------------------------------------------
+//!	描画
+//---------------------------------------------------------------------------------
+void CandyBomb::Draw()
+{
+    __super::Draw();
+}
+
+//---------------------------------------------------------------------------------
+//!	終了
+//---------------------------------------------------------------------------------
+void CandyBomb::Exit()
+{
+    __super::Exit();
+}
+
+//!GUI表示
+void CandyBomb::GUI()
+{
+    __super::GUI();
+}
--- src/Game/Scene/ScenePlay.cpp
+++ src/Game/Scene/ScenePlay.cpp
@@ -17,6 +17,7 @@
 #include "Block.h"
 #include "UFO.h"
 #include "Wall.h"
+#include "CandyBomb.h"
 
 //---------------------------------------------------------------------------------
 //!	初期化
@@ -40,6 +41,10 @@ bool ScenePlay::Init()
 
     for(int i = 0; i < BLOCK_NUM_MAX_; i++) {
         auto block = Scene::Object::Create<Block>();
+    }
+
+    for(int i = 0; i < CANDYBOMB_NUM_MAX_; i++) {
+        auto candybomb = Scene::Object::Create<CandyBomb>();
     }
 
     auto ufo = Scene::Object::Create<UFO>();
