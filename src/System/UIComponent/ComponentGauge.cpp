3ac7f894ecdfd37d431236942fbed23514664f92
504-14\cre <a>
2025-11-01 17:03:20 +0900

    [Add]GaugeComponent

--- src/System/UIComponent/ComponentGauge.cpp
+++ src/System/UIComponent/ComponentGauge.cpp
@@ -1,16 +1,16 @@
 ﻿//---------------------------------------------------------------------------
-//!	@file	ComponentImage.cpp
+//!	@file	ComponentGauge.cpp
 //! @brief	画像コンポーネント
 //! @auther 山﨑愛
 //---------------------------------------------------------------------------
-#include "ComponentImage.h"
+#include "ComponentGauge.h"
 #include "ComponentTransformUI.h"
 #include <Game/system/ImageBuffer.h>
 
 //---------------------------------------------------------------------------
 //! @brief	初期化関数
 //---------------------------------------------------------------------------
-void ComponentImage::Init()
+void ComponentGauge::Init()
 {    // 初期化処理
     __super::Init();
     //---------------------------------------------------------------------------
@@ -24,9 +24,6 @@ void ComponentImage::Init()
         float3 scale      = owner->GetScaleAxisXYZ();
         //サイズは、Transformの平均
         float size = (scale.x + scale.y + scale.z) / 3.0f;    // 平均値をとる
-        //角度はx軸の角度から取る
-        float angle = owner->GetRotationAxisXYZ().x;    // X軸の角度を取得0
-        DrawRotaGraphF(pos.x, pos.y, static_cast<double>(size), static_cast<double>(angle), image_, TRUE);
     };
     SetProc("UIDraw", draw_ui, ProcTiming::UI, static_cast<ProcPriority>(NONE));
 }
@@ -34,7 +31,7 @@ void ComponentImage::Init()
 //---------------------------------------------------------------------------
 //! @brief	ImGui
 //---------------------------------------------------------------------------
-void ComponentImage::GUI()
+void ComponentGauge::GUI()
 {
     __super::GUI();
 
@@ -42,7 +39,7 @@ void ComponentImage::GUI()
     ImGui::Begin(GetOwner()->GetName().data());
     {
         ImGui::Separator();
-        if(ImGui::TreeNode(u8"画像コンポーネント")) {
+        if(ImGui::TreeNode(u8"ゲージコンポーネント")) {
             if(ImGui::Button(u8"削除"))
                 GetOwner()->RemoveComponent(shared_from_this());
 
@@ -53,89 +50,68 @@ void ComponentImage::GUI()
 }
 
 //---------------------------------------------------------------------------
-//! @brief	画像の設定
+//! @brief ゲージの割合を設定する関数
 //---------------------------------------------------------------------------
-std::shared_ptr<ComponentImage> ComponentImage::SetImage(int image)
+std::shared_ptr<ComponentGauge> ComponentGauge::SetGaugeSize(const int2& size)
 {
-    image_ = image;    // 画像ハンドルを設定
-    return dynamic_pointer_cast<ComponentImage>(shared_from_this());
+    gauge_size_ = size;    // ゲージのサイズを設定
+    return dynamic_pointer_cast<ComponentGauge>(shared_from_this());
 }
 
 //---------------------------------------------------------------------------
-//画像ハンドルの取得
+//! @brief ゲージの割合を設定する関数
 //---------------------------------------------------------------------------
-int ComponentImage::GetImageHandle()
+std::shared_ptr<ComponentGauge> ComponentGauge::SetGaugeRate(float rate)
 {
-    return image_;
+    gauge_rate_ = rate;    // ゲージの割合を設定
+    return dynamic_pointer_cast<ComponentGauge>(shared_from_this());
 }
 
 //---------------------------------------------------------------------------
-//! @brief	画像のサイズを取得する関数
+//! @brief ゲージの色を設定する関数
 //---------------------------------------------------------------------------
-float2 ComponentImage::GetImageSize() const
+std::shared_ptr<ComponentGauge> ComponentGauge::SetGaugeColor(float color)
 {
-    float width = 0;                          // 幅
-    float hight = 0;                          //高さ
-    GetGraphSizeF(image_, &width, &hight);    // 画像のサイズを取得
-    return float2(width, hight);              // 画像のサイズをfloat2で返す
+    color_ = color;    // ゲージの色を設定
+    return dynamic_pointer_cast<ComponentGauge>(shared_from_this());
 }
 
 //---------------------------------------------------------------------------
-//! @brief	画面にうつる画像のサイズを取得する関数
+//! @brief  ゲージ座標の補正値を取得する関数
 //---------------------------------------------------------------------------
-float2 ComponentImage::GetScreenImageSize()
-{
-    float width = 0;                          // 幅
-    float hight = 0;                          //高さ
-    GetGraphSizeF(image_, &width, &hight);    // 画像のサイズを取得
-    auto   owner = GetOwner();                //オーナーを取得
-    float3 scale = owner->GetScaleAxisXYZ();
-    //サイズは、Transformの平均
-    float size  = (scale.x + scale.y + scale.z) / 3.0f;    // 平均値をとる
-    width      *= size;                                    // 幅にサイズをかける
-    hight      *= size;                                    // 高さにサイズをかける
-    return float2(width, hight);                           // 画像のサイズをfloat2で返す
-}
-
-//---------------------------------------------------------------------------
-//! @brief  画像座標の補正値を取得する関数
-//---------------------------------------------------------------------------
-float3 ComponentImage::GetAdjustment() const
+float3 ComponentGauge::GetAdjustment() const
 {
     auto   owner      = GetOwner();                  //オーナーを取得
     float3 adjustment = float3(0.0f, 0.0f, 0.0f);    // 調整値(Alignmentに合わせて)
     if(auto comp_transform = owner->GetComponent<ComponentTransformUI>()) {
         ComponentTransformUI::Alignment alignment = comp_transform->GetAlignment();
-        float                           width     = 0;    // 幅
-        float                           hight     = 0;    //高さ
-        GetGraphSizeF(image_, &width, &hight);
         //配置位置(縦)
         switch(static_cast<int>(alignment) / 3) {
         case 0:
-            adjustment.y = (-hight * 0.5f);
+            adjustment.y = (-gauge_size_.y * 0.5f);
             break;    // 上寄せ
         case 1:
             adjustment.y = 0.0f;
             break;    // 中央寄せ
         case 2:
-            adjustment.y = (hight * 0.5f);
+            adjustment.y = (gauge_size_.y * 0.5f);
             break;    // 下寄せ
         }
         //配置位置(横)
         switch(static_cast<int>(alignment) % 3) {
         case 0:
-            adjustment.x = (-width * 0.5f);
+            adjustment.x = (-gauge_size_.x * 0.5f);
             break;    // 左寄せ
         case 1:
             adjustment.x = 0.0f;
             break;    // 中央寄せ
         case 2:
-            adjustment.x = (width * 0.5f);
+            adjustment.x = (gauge_size_.x * 0.5f);
             break;    // 右寄せ
         }
     }
     return adjustment;
 }
 
-CEREAL_REGISTER_TYPE(ComponentImage)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(Component, ComponentImage)
+CEREAL_REGISTER_TYPE(ComponentGauge)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(Component, ComponentGauge)
---
+++ src/System/UIComponent/ComponentGauge.h
@@ -0,0 +1,66 @@
+﻿#pragma once
+//---------------------------------------------------------------------------
+//!	@file	ComponentGauge.h
+//! @brief	ゲージコンポーネント
+//! @auther 山﨑愛
+//---------------------------------------------------------------------------
+#include <System/Scene.h>
+#include <System/Component/Component.h>
+
+USING_PTR(ComponentGauge);
+
+class ComponentGauge : public Component
+{
+public:
+    BP_COMPONENT_DECL(ComponentGauge, u8"ゲージコンポーネント");
+    //! @{
+    //	初期化処理
+    void Init() override;
+
+    //ImGui
+    void GUI() override;
+
+    //---------------------------------------------------------------------------
+    // ゲージの割合を設定する関数
+    //! @param size [in] ゲージの幅と高さ
+    //! @retval 自身のポインタ
+    //---------------------------------------------------------------------------
+    std::shared_ptr<ComponentGauge> SetGaugeSize(const int2& size);
+
+    //---------------------------------------------------------------------------
+    // ゲージの割合を設定する関数
+    //! @param rate [in] ゲージの割合(0.0~1.0)
+    //! @retval 自身のポインタ
+    //---------------------------------------------------------------------------
+    std::shared_ptr<ComponentGauge> SetGaugeRate(float rate);
+
+    //---------------------------------------------------------------------------
+    // ゲージの色を設定する関数
+    //! @param color [in] ゲージの色(16進数で)
+    //! @retval 自身のポインタ
+    //---------------------------------------------------------------------------
+    std::shared_ptr<ComponentGauge> SetGaugeColor(float color);
+
+    //---------------------------------------------------------------------------
+    //	ゲージ座標の補正値を取得する関数
+    //! @retval ゲージ座標の補正値
+    //---------------------------------------------------------------------------
+    float3 GetAdjustment() const;
+
+private:
+    float2 gauge_size_ = float2(100.0f, 20.0f);    // ゲージのサイズ(幅と高さ)
+    float  gauge_rate_ = 1.0f;                     // ゲージの割合(0.0~1.0)
+    int    color_      = GetColor(255, 0, 0);      // ゲージの色
+
+    //--------------------------------------------------------------------
+    //! @name Cereal処理
+    //--------------------------------------------------------------------
+    //@{
+
+    //! @brief セーブ
+    // @param arc アーカイバ
+    // @param ver バージョン
+    CEREAL_SAVELOAD(arc, ver) { arc(cereal::make_nvp("Component", cereal::base_class<Component>(this))); }
+};
+
+CEREAL_CLASS_VERSION(ComponentGauge, 3);
