ab4e565a786e9bad3a37737a8a64069cfea3fbf1
504-13\cre <a>
2025-11-08 20:05:53 +0900

    正面方向を取得 真上に飛ぶからその処理はコメントアウト

---
+++ src/Game/Scene/SkillObject/FireBall.cpp
@@ -0,0 +1,125 @@
+﻿//---------------------------------------------------------------------------
+//!	@file	FireBall.cpp
+//! @brief	ゲームシーンの火球オブジェクト
+//! @auther 田中南々子
+//---------------------------------------------------------------------------
+#include "FireBall.h"
+#include <System/Component/ComponentCollisionSphere.h>
+#include <System/Component/ComponentStatus.h>
+#include <System/Component/ComponentRigidbody.h>
+
+//---------------------------------------------------------------------------------
+//!	初期化
+//---------------------------------------------------------------------------------
+bool FireBall::Init()
+{
+    __super::Init();
+
+    SetName(u8"火球");
+    SetEffect(u8"data/PoyPoy/Effect/FireBall/FireBall1.efkefc");
+    //---------------------------------------------------------------------------------
+    // コリジョンを追加する
+    //---------------------------------------------------------------------------------
+    auto col_comp = AddComponent<ComponentCollisionSphere>();
+    col_comp->SetRadius(1.2f);
+    col_comp->SetCollisionGroup(ComponentCollision::CollisionGroup::SKILL);    // 所属するグループを「SKILL」とします
+    //PLAYERとGROUND、ETCに当たるように設定
+    col_comp->SetHitCollisionGroup(static_cast<u32>(ComponentCollision::CollisionGroup::PLAYER) | static_cast<u32>(ComponentCollision::CollisionGroup::GROUND) |
+                                   static_cast<u32>(ComponentCollision::CollisionGroup::ETC));
+
+    col_comp->SetOverlapCollisionGroup(0xffffffff);    //すべてオーバーラップ(押し戻さないが、OnHitにコールバックは行うように)するように設定
+    collision_component_ = col_comp;
+
+    //リジッドボディを追加
+    auto body = AddComponent<ComponentRigidbody>();
+    body->SetMass(10.0f);    //質量を設定
+
+    radius_ = 0.0f;
+
+    return true;
+}
+
+void FireBall::Update()
+{
+    __super::Update();
+    //当たったら消える
+    if(is_hit_) {
+        //エフェクトを切り替える
+        if(effect_changed_ == false) {
+            // エフェクト切替
+            SetEffect(u8"data/PoyPoy/Effect/FireBall/FireBall2.efkefc");
+            effect_changed_ = true;
+
+            // 当たり後は物理力の付与など不要なので早期リターン
+            return;
+        }
+
+        // 生成直後に重力を無効化する例
+        if(auto rb = GetComponent<ComponentRigidbody>())
+            rb->SetUseGravity(false);
+    }
+    else {
+        ////前方向を取得
+        ////リジッドボディを取得
+        //auto col			   = GetComponent<ComponentCollisionSphere>();	   //コリジョンを取得
+        //auto body			   = GetComponent<ComponentRigidbody>();		  //リジッドボディを追加
+
+        //float3 throw_impulse  = float3(0.0f, THROW_VIRTICAL_POWER_, 0.0f);	  //投げる力の初期化
+        //direction_.y		   += 180.0f;										//座標系の関係でyを180度回転する、オブジェクトの背中が正面
+
+        //body->SetMass(6.0f);	//物体の質量を設定
+
+        ////オーナーのy軸回転から、throw_impulse_のxとzを設定
+        //throw_impulse.x = -THROW_HORIZONTAL_POWER_ * sinf(D2R(direction_.y));
+        //throw_impulse.z = -THROW_HORIZONTAL_POWER_ * cosf(D2R(direction_.y));
+
+        //body->AddImpulse(throw_impulse);	//投げる力を加える
+        //col->SetEnableFlag(true);			//コリジョンを有効にする
+        //col->UseGravity();					//重力を使用する
+    }
+}
+
+void FireBall::SetDirection(float3 direction)
+{
+    direction_ = direction;
+}
+
+//---------------------------------------------------------------------------
+//! @brief	当たった情報はコールバックで送られてくる
+//---------------------------------------------------------------------------
+void FireBall::OnHit(const ComponentCollision::HitInfo& hit_info)
+{
+    __super::OnHit(hit_info);
+    //キャラクターが当たったらダメージを受ける
+    if(auto other_owner = hit_info.hit_collision_->GetOwner()) {
+        //当たったオブジェクトの名前が使用者か確認
+        if(other_owner->GetName() == skill_owner_name_) {
+            //使用者なので何もしない
+            return;
+        }
+        //ステータスコンポーネントを取得
+        if(auto status_comp = other_owner->GetComponent<ComponentStatus>()) {
+            //ダメージを与える(とりあえず4)
+            status_comp->TakeDamage(ATTACK_DAMAGE_);
+        }
+        //当たったフラグを立てる
+        is_hit_ = true;
+
+        //コリジョンを膨らませる
+        if(auto collision = collision_component_.lock()) {
+            collision->SetRadius(RADUIS_ * 4.0f);
+        }
+
+        //ヒットした相手方の剛体に力を加える
+        if(auto other = hit_info.hit_collision_->GetOwner()) {
+            if(auto other_rb = other->GetComponent<ComponentRigidbody>()) {
+                //自分から相手への方向ベクトルを計算
+                float3 direction = other->GetTranslate() - GetTranslate();
+                direction        = normalize(direction);
+                //力を加える
+                const float force_strength = 400.0f;
+                other_rb->AddImpulse(direction * force_strength);
+            }
+        }
+    }
+}
--- src/System/SkillComponent/ComponentFireBall.cpp
+++ src/System/SkillComponent/ComponentFireBall.cpp
@@ -4,6 +4,7 @@
 //! @auther 山﨑愛
 //---------------------------------------------------------------------------
 #include "ComponentFireBall.h"
+#include <Game/Scene/SkillObject/FireBall.h>
 #include <System/Component/ComponentEffect.h>
 
 //---------------------------------------------------------------------------
@@ -54,17 +55,24 @@ std::shared_ptr<ComponentSkill> ComponentFireBall::UseSkill()
 {
     __super::UseSkill();
     auto owner = GetOwner();
-    //ファイルパス
-    //const std::string eff_name = "data/PoyPoy/Effect/Fireball/FireBall1.efkefc";
-    const std::string eff_name = "data/PoyPoy/Effect/Fireball/FireBall2.efkefc";
+    //---------------------------------------------------------------------------
+    // スキルオブジェクトの生成
+    //---------------------------------------------------------------------------
+    auto fire_ball = Scene::Object::Create<FireBall>();    //ポイズンオブジェクトを生成
+
+    fire_ball->SetSkillOwnerName(owner->GetName());    //スキル使用者の名前を設定(スキルが使用者に干渉しなくするために設定不可欠。)
     //高さの値は仮、後に持ち上げているオブジェクトの位置に変更するはず。
-    const float3 pos    = owner->GetTranslate() + float3(0.0f, 20.0f, 0.0f);
-    auto         effect = ComponentEffect::Object::Create(eff_name, pos);
-    //プレイヤーの向きに合わせる
-    effect->SetRotationAxisXYZ(owner_->GetRotationAxisXYZ());
+    const float3 pos = owner->GetTranslate() + float3(0.0f, 20.0f, 0.0f);
+    fire_ball->SetTranslate(pos);
+
+    auto rotate = owner->GetRotationAxisXYZ();
+
+    fire_ball->SetDirection(rotate);    // 進行方向を設定
+
+    fire_ball->SetScaleAxisXYZ(SCALE_);
+    fire_ball->GetComponent<ComponentEffect>()->SetPlaySpeed(1.0f);
     //自身のポインタを変換
     return dynamic_pointer_cast<ComponentSkill>(shared_from_this());
 }
-
 CEREAL_REGISTER_TYPE(ComponentFireBall)
 CEREAL_REGISTER_POLYMORPHIC_RELATION(Component, ComponentFireBall)
