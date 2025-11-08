68463e1b9b8fe0487dfc87034193226e013deca5
504-20\cre <a>
2025-11-08 18:18:06 +0900

    コンフリクト解消

--- src/System/Component/ComponentStatus.h
+++ src/System/Component/ComponentStatus.h
@@ -54,10 +54,11 @@ private:
     //デルタタイム
     std::chrono::time_point<std::chrono::steady_clock> prev_time_    = std::chrono::high_resolution_clock::now();    //前フレームの時間
     std::chrono::time_point<std::chrono::steady_clock> current_time_ = std::chrono::high_resolution_clock::now();    //現在の時間
-    //HP関連
-    int   hp_                  = 0;       //ヒットポイント
-    float INVINCIBILITY_TIME_  = 2.0f;    //無敵時間
-    float invincibility_timer_ = 0.0f;    //無敵時間タイマー
+                                                                                                                     //HP関連
+    int   max_hp_              = 200;                                                                                //最大ヒットポイント
+    int   hp_                  = 0;                                                                                  //ヒットポイント
+    float INVINCIBILITY_TIME_  = 2.0f;                                                                               //無敵時間
+    float invincibility_timer_ = 0.0f;                                                                               //無敵時間タイマー
     //スピード関連
     float move_speed_ = 0.2f;    //移動スピード
 
