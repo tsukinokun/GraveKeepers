83557cd4ec7fd4d50c5aa4233d44c2ec7e51afa9
504-18\cre <a>
2025-11-15 19:15:13 +0900

    リザルト画面の作成

---
+++ src/Game/Scene/GameResult.h
@@ -0,0 +1,27 @@
+﻿//---------------------------------------------------------------------------
+//!	@file	GameResult.h
+//! @brief	リザルト画面
+//---------------------------------------------------------------------------
+#pragma once
+#include <System/Scene.h>
+
+//===========================================================================
+//! アニメーションサンプルシーン
+//===========================================================================
+class GameResult : public Scene::Base
+{
+public:
+    BP_CLASS_DECL(GameResult, u8"リザルト画面")
+
+    //@}
+    bool Init() override;      //!< 初期化
+    void Update() override;    //!< 更新
+    void Draw() override;      //!< 描画
+    void Exit() override;      //!< 終了
+    void GUI() override;       //!< GUI表示
+
+    int result_back_graph = -1;    //!< リザルト背景画像
+    int spider_web_graph  = -1;    //!< 蜘蛛の巣画像
+
+private:
+};
