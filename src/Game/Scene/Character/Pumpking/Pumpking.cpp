bfae40a16458c86afb1a8a95b111b48252e50a97
504-13\cre <a>
2025-11-29 17:26:44 +0900

    パンプキングのアニメーションの追加、選択画面でのアニメーションはコンフリクトを起こすからいったん待機

--- src/Game/Scene/Character/Pumpking/Pumpking.cpp
+++ src/Game/Scene/Character/Pumpking/Pumpking.cpp
@@ -21,6 +21,18 @@ bool Pumpking::Init()
     __super::Init();
     if(auto model_comp = model_component_.lock()) {
         model_comp->Load("data/PoyPoy/Model/Character/Pumpking/Pumpking.mv1");
+
+        model_comp->SetAnimation({
+            {          "idle",           "data/PoyPoy/Model/Character/Pumpking/Anims/Idle.mv1", 1, 1.0f}, //待機
+            {          "walk",           "data/PoyPoy/Model/Character/Pumpking/Anims/Walk.mv1", 1, 1.0f}, //歩き
+            {           "run",            "data/PoyPoy/Model/Character/Pumpking/Anims/Run.mv1", 1, 1.0f}, //走り
+            {         "death", "data/PoyPoy/Model/Character/Pumpking/Anims/DeathMotionLay.mv1", 1, 1.0f}, //死亡
+            {          "jump",           "data/PoyPoy/Model/Character/Pumpking/Anims/Jump.mv1", 1, 1.0f}, //ジャンプ
+            {"crouching_idle",  "data/PoyPoy/Model/Character/Pumpking/Anims/CrouchingIdle.mv1", 1, 1.0f}, //しゃがみ待機
+            {"crouching_walk",  "data/PoyPoy/Model/Character/Pumpking/Anims/CrouchingWalk.mv1", 1, 1.0f}, //しゃがみ歩き
+            { "crouching_run",   "data/PoyPoy/Model/Character/Pumpking/Anims/CrouchingRun.mv1", 1, 1.0f}, //しゃがみ走り
+            {     "low_crawl",       "data/PoyPoy/Model/Character/Pumpking/Anims/LowCrawl.mv1", 1, 1.0f}, //這いずり移動
+        });
     }
     if(auto status_comp = status_component_.lock()) {
         status_comp->SetHitPoints(HP_MAX_);
